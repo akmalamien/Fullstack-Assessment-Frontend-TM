30f91ec5a91dc4f632a19f3fe61d2c256815289f
akmalamien <akmal.amien@gmail.com>
2023-02-14 02:29:52 +0800

    updated service

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -1,17 +1,32 @@
-import axios from "axios";
+import axios from 'axios';
 
 const HttpService = {
-  get: (url) => {
-    const result = axios.get(url)
-      .then((response) => {         
-          return response        
-        })
-      .catch((error) => {    
-          console.log(error);
-        })
-    
-    return result      
-  }
-}
+  get: url => {
+    const result = axios
+      .get(url)
+      .then(response => {
+        return response;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    return result;
+  },
+
+  post: (url, bodyRequest) => {
+    console.log("hello")
+    const result = axios
+      .post(url, bodyRequest)
+      .then(response => {
+        console.log("response", response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    return result;
+  },
+};
 
 export default HttpService;
