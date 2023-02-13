88cf807b1dd59e73501a00e11faf8873b5284a4f
akmalamien <akmal.amien@gmail.com>
2023-02-14 02:30:21 +0800

    updated actions

--- src/redux/actions/task.js
+++ src/redux/actions/task.js
@@ -1,20 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import {createSlice} from '@reduxjs/toolkit';
 const taskSlice = createSlice({
-  name: "task",
+  name: 'task',
   initialState: {
-    tasks: []
+    tasks: [],
   },
   reducers: {
-    storeTask: (state, action) => {        
-        state.tasks = action.payload.data   
+    storeTask: (state, action) => {
+      state.tasks = action.payload.data;
     },
-    
     removeAllTask: state => {
-      state.tasks = []
-    }
-  }
+      state.tasks = [];
+    },
+  },
 });
-
-export const { storeTask, removeAllTask } = taskSlice.actions;
+export const {storeTask, removeAllTask} = taskSlice.actions;
 export default taskSlice.reducer;
