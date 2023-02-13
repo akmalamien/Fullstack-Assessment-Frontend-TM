29c6fa86fb1f92a97e169b2e85e203110a6b9eeb
akmalamien <akmal.amien@gmail.com>
2023-02-14 02:30:33 +0800

    updated App.js

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
-import React from "react";
-import { Provider } from "react-redux";
-import HomeScreen from "./src/screens/HomeScreen";
-import store from "./src/redux/store";
-import AddTaskScreen from "./src/screens/AddTaskScreen";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from 'react';
+import { Provider } from 'react-redux';
+import HomeScreen from './src/screens/HomeScreen';
+import store from './src/redux/store';
+import AddTaskScreen from './src/screens/AddTaskScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const App = () => {
-  const Stack = createNativeStackNavigator()
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Screen name="Home" component={HomeScreen} />     
-        {/* <Stack.Screen name="AddTask" component={AddTaskScreen} />      */}
-      </NavigationContainer>      
+        <Stack.Navigator>
+          <Stack.Screen name="AddTask" component={AddTaskScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
